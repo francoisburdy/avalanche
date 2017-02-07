312c9ce54bc306f4b600e8e4e3ce3e412754551e
francoisburdy <francois.burdy@gmail.com>
2017-02-07 12:24:10 +0100

    retrofit avec boutons floating & font-awesome (voir avec alexis pourquoi addIntervenant est HS)

--- www/modules/historique/historique.js
+++ www/modules/historique/historique.js
@@ -1,5 +1,13 @@
 'use strict';
 
 angular.module('myApp').controller('HistoriqueCtrl', function($scope, Operation) {
+	$scope.journaux = Operation.getJournaux();
 
+	$scope.isSelected = function(index) {
+		return $scope.selected == index;
+	}
+
+	$scope.select = function(index) {
+		$scope.selected = index;
+	}
 });
--- www/modules/services/operation_service.js
+++ www/modules/services/operation_service.js
@@ -1,4 +1,4 @@
-angular.module('myApp').service('Operation', function($localStorage, $rootScope) {
+angular.module('myApp').service('Operation', function($localStorage, $rootScope, $filter) {
     
     this.getOperation = function() {
         return $localStorage.operation;
@@ -7,7 +7,7 @@ angular.module('myApp').service('Operation', function($localStorage, $rootScope)
     this.createOperation = function() {
         if($localStorage.operation == null) {
             $localStorage.operation = {
-                nom: 'Avalanche du ' + new Date(),
+                nom: 'Avalanche du ',
                 beginDate: new Date(),
                 endDate: null,
                 personnels: [],
@@ -27,6 +27,70 @@ angular.module('myApp').service('Operation', function($localStorage, $rootScope)
         $rootScope.$broadcast('operationUpdated');
     }
 
+    this.getJournaux = function() {
+        var journaux = [];
+
+        for(var i = 0; i < $localStorage.historique.length; i++) {
+            journaux.push(this.getJournal($localStorage.historique[i]));            
+        }
+        return journaux;
+    }
+
+    /*********************************
+     *       ToDO
+     *       RAJOUTER LE METIER
+     *********************************/
+    this.getJournal = function(operation) {
+        var journal = {
+            nom: operation.nom,
+            beginDate: operation.beginDate,
+            endDate: operation.endDate,
+            nbVictimes: operation.victimes.length,
+            nbPersonnels : operation.personnels.length,
+            evenements: []
+        };
+        for(var i = 0; i < operation.victimes.length; i++) {
+            var v = operation.victimes[i];
+            
+            var evDebut = {
+                date: v.beginDate,
+                texte: 'La victime n°' + v.numero + ' a été découverte à ' + $filter('date')(v.beginDate, 'hh:mm le dd/MM/yyyy') + '.',
+                type: 'entrée'
+            };
+            journal.evenements.push(evDebut);
+
+            if(v.endDate != null) {
+                var evFin = {
+                    date: v.endDate,
+                    texte: 'La victime n°' + v.numero + ' a été évacuée à ' + $filter('date')(v.endDate, 'hh:mm le dd/MM/yyyy') + '.',               
+                    type: 'sortie'
+                };
+                journal.evenements.push(evFin);
+            }
+            
+        }
+        for(var i = 0; i < operation.personnels.length; i++) {
+            var p = operation.personnels[i];
+
+            var evDebut = {
+                date : v.entryDate,
+                texte : 'Le ' + /**** METIER ****/ ' numéro ' + p.numero + ' est entré sur zone à '+ $filter('date')(p.entryDate, 'hh:mm le dd/MM/yyyy') + '.',
+                type: 'entrée'  
+            };
+            journal.evenements.push(evDebut);
+
+            if(p.exitDate != null) {
+                var evFin = {
+                    date : v.exitDate,
+                    texte : 'Le ' + /**** METIER ****/ ' numéro '+ p.numero +' est sortie de la zone à '+ $filter('date')(p.exitDate, 'hh:mm le dd/MM/yyyy') + '.',
+                    type: 'sortie'
+                }
+                journal.evenements.push(evFin);
+            }
+        }
+        return journal;
+    }
+
     this.pushHistorique = function(operation) {
         if(!$localStorage.historique) {
             $localStorage.historique = [];
@@ -38,24 +102,36 @@ angular.module('myApp').service('Operation', function($localStorage, $rootScope)
     this.addPersonnel = function(personnel) {
         if($localStorage.operation.personnels.indexOf(personnel) !== -1) {
             console.log("Error : operation already contains this personnel");
-        } else $localStorage.operation.personnels.push(personnel);
+        } else {
+            $localStorage.operation.personnels.push(personnel);
+            $rootScope.$broadcast('operationUpdated');
+        }
     }
 
     this.removePersonnel = function(personnel) {
         var index = $localStorage.operation.personnels.indexOf(personnel);
         if(index == -1) console.log("Error : Operation does not contain this personnel");
-        else $localStorage.operation.personnels.splice(index, 1);
+        else {
+            $localStorage.operation.personnels.splice(index, 1);   
+            $rootScope.$broadcast('operationUpdated');
+        }
     }
 
     this.addVictime = function(victime) {
         if($localStorage.operation.victimes.indexOf(victime) !== -1) console.log("Error : Operation already contains this victime");
-        else $localStorage.operation.victimes.push(victime);
+        else {
+            $localStorage.operation.victimes.push(victime);
+            $rootScope.$broadcast('operationUpdated');
+        }
     }
 
     this.removeVictime = function(victime) {
         var index = $localStorage.operation.victimes.indexOf(victime);
         if(index == -1) console.log("Error : Operation does not contain this victime");
-        else $localStorage.operation.victimes.splice(index, 1);
+        else {
+            $localStorage.operation.victimes.splice(index, 1);
+            $rootScope.$broadcast('operationUpdated');
+        }
     }
 
     this.generateVictimeNumber = function() {
@@ -68,5 +144,4 @@ angular.module('myApp').service('Operation', function($localStorage, $rootScope)
             return nextNumber;
         }
     }
-
 });
