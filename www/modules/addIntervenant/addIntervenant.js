5298d89aa812fa6a5338ab23dfec9c67bf520e25
arnaud fontaine <arnaud fontaine>
2017-02-07 15:27:26 +0100

    photo intervenant (chantier)

--- www/modules/addIntervenant/addIntervenant.js
+++ www/modules/addIntervenant/addIntervenant.js
@@ -2,11 +2,11 @@
 
 angular.module('myApp').controller('AddIntervenantCtrl', function($scope, $location, Operation, Parametres) {
 	$scope.newIntervenant = {};
-	$scope.newIntervenant.numero = Operation.generateIntervenantNumber();
+	//$scope.newIntervenant.numero = Operation.generateIntervenantNumber();
 
-	$scope.IntervenantStatus = Parametres.getIntervenantStatus();
+	//$scope.IntervenantStatus = Parametres.getIntervenantStatus();
 
-	$scope.IntervenantPhoto= null;
+	//$scope.IntervenantPhoto= null;
 
   	$scope.addIntervenant = function() {
   		$scope.newIntervenant.beginDate = new Date();
@@ -14,6 +14,32 @@ angular.module('myApp').controller('AddIntervenantCtrl', function($scope, $locat
   		Operation.addPersonnel($scope.newIntervenant);
   		$location.url('/dashboard');
   	}
+
+     $scope.launchCamera = function() {
+        console.log(navigator.camera);
+        if(navigator.camera !== undefined) {
+            navigator.camera.getPicture(onSuccess, onFail, { quality: 50, destinationType: Camera.DestinationType.DATA_URL });
+        }
+    }
+    
+    function onSuccess(imageData) {
+        console.log('image : onSuccess');
+        console.log(imageData);
+        storage.setItem('imageData', imageData);
+        refreshImageContent(imageData);     
+    }
+    
+    function onFail(message) {
+        console.log('image : onFail');
+    }
+
+    function refreshImageContent(imageData) {
+        if(imageData != undefined) {
+            var image = document.getElementById('imagePreview') ;
+            image.src = "data:image/jpeg;base64," + imageData;
+        }
+    }
+
 
 
 });
