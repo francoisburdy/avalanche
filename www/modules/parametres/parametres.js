d2a1a09d30d559689476dd3dff10d497b978acb7
Adrien Mercier <adrieniar74@gmail.com>
2017-02-08 16:42:03 +0100

    color picker colors

--- www/modules/parametres/parametres.js
+++ www/modules/parametres/parametres.js
@@ -27,7 +27,9 @@ angular.module('myApp').controller('ParametresCtrl', function($scope, Parametres
 		
 	}
 	
-	$scope.options = ['transparent','#FF8A80', '#FFD180', '#FFFF8D', '#CFD8DC', '#80D8FF', '#A7FFEB', '#CCFF90'];
+	$scope.options = ['#FFF', '#FFA773', '#FFD180', '#FFFF8D', 
+					'#CFD8DC', '#80D8FF', '#A7FFEB', '#E969A8', 
+					'#6996D3', '#CCFF90', '#5FD2B5', '#FF8A80'];
     // $scope.color = '#FF8A80';
 
     $scope.colorChanged = function(newColor, oldColor) {
