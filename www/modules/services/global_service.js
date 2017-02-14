da0f8512ba604b26e1426f6264e2d31b776bf13e
francoisburdy <francois.burdy@gmail.com>
2017-02-14 16:43:43 +0100

    loading données de démo

--- www/modules/services/global_service.js
+++ www/modules/services/global_service.js
@@ -1,4 +1,4 @@
-﻿angular.module('myApp').service('Global', function($localStorage, $rootScope, $location) {
+﻿angular.module('myApp').service('Global', function($localStorage, $rootScope, $location, $http) {
     
     /**
      * Ferme l'application
@@ -29,7 +29,20 @@
     }
 
     this.loadDemoData = function() {
-        // TODO : charger données de démo
+        $http.get('demo/demo-historique.json').then(function(res) {
+            console.log("historique", res);
+            if(res.status == 200 && res.data)
+                $localStorage.historique = res.data;                
+        });
+
+        $http.get('demo/demo-operation.json').then(function(res) {
+            console.log("operation", res);
+            if(res.status == 200 && res.data)
+                $localStorage.operation = res.data;                
+        });
+
+        // TODO : faire une redirection sur dashboard après chargement terminé (promise ?)
+
     }
 
 });
