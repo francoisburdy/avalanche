fc84c3dd6c0e05ee7d559c3b34ab4c8b5244298c
Adrien Mercier <adrieniar74@gmail.com>
2017-02-15 14:33:16 +0100

    pdf translation

--- www/modules/services/export_service.js
+++ www/modules/services/export_service.js
@@ -22,13 +22,13 @@ angular.module('myApp').service('Export', function($filter, Operation) {
 
     this.historiqueAsHtml = function($scope) {
         var operations = Operation.getJournaux();
-        var now = $filter('date')(new Date(), 'dd/MM/yyyy à HH:mm');
+        var now = $filter('date')(new Date(), $scope.translation.dateFormat + ' ' + $scope.translation.journal.at + ' ' + $scope.translation.hourFormat);
 
         var html = '<!doctype html><html lang="fr"><head>';
         html += '<style type="text/css">body{margin:1.8cm 2.3cm}</style>';
         html += '</head>';
         html += '<body><h1>' + $scope.translation.export.historyReportTitle + '</h1>';
-        html += '<div style="text-align:right"><em>Généré le '+ now +'</em></div>';
+        html += '<div style="text-align:right"><em>' + $scope.translation.export.generated + ' ' + now +'</em></div>';
         for (let o of operations){
             html += this.operationAsHtml(o, $scope) + "<br /><hr />";
         }
@@ -39,13 +39,13 @@ angular.module('myApp').service('Export', function($filter, Operation) {
 
     this.currentOperationAsHtml = function($scope) {
         let o = Operation.getCurrentJournal();
-        var now = $filter('date')(new Date(), 'dd/MM/yyyy à HH:mm');
+        var now = $filter('date')(new Date(), $scope.translation.dateFormat + ' ' + $scope.translation.journal.at + ' ' + $scope.translation.hourFormat);
 
         var html = '<!doctype html><html lang="fr"><head>';
         html += '<style type="text/css">body{margin:1.8cm 2.3cm}</style>';
         html += '</head>';
-        html += '<body><h1>Rapport temporaire d\'opération</h1>';
-        html += '<div style="text-align:right"><em>Généré le '+ now +'</em></div>';
+        html += '<body><h1>' + $scope.translation.export.temporaryReport + '</h1>';
+        html += '<div style="text-align:right"><em>' + $scope.translation.export.generated ' ' + now +'</em></div>';
 		html += this.operationAsHtml(o, $scope) + "<br /><hr />";
         html += "</body></html>";
 
@@ -53,22 +53,22 @@ angular.module('myApp').service('Export', function($filter, Operation) {
     }
 
     this.operationAsHtml = function(o, $scope) {
-        var date = $filter('date')(o.beginDate, 'dd/MM/yyyy');
-        var dateDebut = $filter('date')(o.beginDate, 'dd/MM/yyyy, HH:mm');
-        var dateFin = $filter('date')(o.endDate, 'dd/MM/yyyy, HH:mm');
-        var heureFin = $filter('date')(o.endDate, 'HH:mm');
-        var heureNow = $filter('date')(new Date(), 'HH:mm');
+        var date = $filter('date')(o.beginDate, $scope.translation.dateFormat);
+        var dateDebut = $filter('date')(o.beginDate, $scope.translation.dateFormat + ', ' + $scope.translation.hourFormat);
+        var dateFin = $filter('date')(o.endDate, $scope.translation.dateFormat + ', ' + $scope.translation.hourFormat);
+        var heureFin = $filter('date')(o.endDate, $scope.translation.hourFormat);
+        var heureNow = $filter('date')(new Date(), $scope.translation.hourFormat);
 
-        var html = "<div><div><h3>"+o.nom+" "+date+"</h3><div>"; //titre + debut details operation
-        html += '<p>Opération débutée le '+dateDebut+ '.</p>';
-        html += '<p>Cette opération a impliqué '+o.nbVictimes+' victimes et '+o.nbPersonnels+' personnels.</p>';
+        var html = "<div><div><h3>" + o.nom + " " + date + "</h3><div>"; //titre + debut details operation
+        html += '<p>' + $scope.translation.journal.operationStarted + ' ' + dateDebut + '.</p>';
+        html += '<p>' + $scope.translation.journal.implicated + ' ' + o.nbVictimes + ' ' + $scope.translation.export.victimesAnd + ' ' + o.nbPersonnels + ' ' + $scope.translation.export.personnels + '.</p>';
 
-        for(let evt of o.evenements) html += '<div>'+evt.texte+'</div>';
+        for(let evt of o.evenements) html += '<div>' + evt.texte + '</div>';
         
         if(heureFin)
-        	html += "<p><em>L'opération à été marquée comme terminée à " + heureFin + "</em></p>";
+        	html += "<p><em>" + $scope.translation.export.marquedFinished + " " + heureFin + "</em></p>";
         else 
-        	html += "<p><em>&Agrave; " + heureNow + ", l'opération n'est pas marquée comme terminée.</em></p>";
+        	html += "<p><em>" + $scope.translation.export.at + " " + heureNow + ", " + $scope.translation.export.notMarquedFinished + ".</em></p>";
 
         html += "</div>";
         return html;
