209852c2e9bee3b85af021eb377faca4bcd58370
arnaud fontaine <arnaud fontaine>
2017-02-16 17:11:10 +0100

    doc detailsMetier.js

--- www/modules/intervenants/detailsMetier.js
+++ www/modules/intervenants/detailsMetier.js
@@ -6,18 +6,30 @@
  * @name DetailsMetierCtrl
  */
 angular.module('myApp').controller('DetailsMetierCtrl', function($scope, $routeParams, $location, Operation, Translation) {
-    
+    /**
+     * Récupère les informations stockées localement sur le métier et les intervenants qui y sont associés
+     * @memberof DetailsMetierCtrl
+     * @function init
+     */
     function init() {
         Translation.getTranslation($scope);
         $scope.metier = $routeParams.lib; 
         $scope.personnels = Operation.getPersonnelsByMetier($routeParams.lib);
     }
     init();
-
+    /**
+     * Affiche les détails sur un intervenant
+     * @memberof DetailsMetierCtrl
+     * @function seeDetails
+     */
     $scope.seeDetails = function(num) {
     	$location.url('/intervenants/' + num);
     }
-
+    /**
+     * Charge la page pour aajouter un intervenant à un metier
+     * @memberof DetailsMetierCtrl
+     * @function addIntervenantMetier
+     */
     $scope.addIntervenantMetier = function(lib) {
         $location.url('/addIntervenantMetier/' + lib);
     }
